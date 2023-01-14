e5e83f688b6df2365e0e83cc3b5e57b91aed62ef
CanaanGM <reakak1@gmail.com>
2023-01-14 03:52:57 +0300

    plan in place

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// get data from questioning user 
+    // find a way to hide secret contact key per session or user
+    // sanitize gotten data, never trust a random PoorFuckingInfantry
+
+// contact space wizrad with a user query
+// get data from said wizard
+    // decode it and ? format it ?
+// display data on bash with colors if possible 
+
+fn main() {
+    println!("Hello, world!");
+}
